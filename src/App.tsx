919f74e8ac07915fcf9cf8845930b3850afdf6d5
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-07-20 22:45:31 +0900

    Feat: api test page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { requestToGemini } from "./util/gemini";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const res = await requestToGemini(text);
+    setResponse(res);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Type Message</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <button type="submit">전송</button>
+      </form>
+      <p>
+        {response}
       </p>
     </>
   )
